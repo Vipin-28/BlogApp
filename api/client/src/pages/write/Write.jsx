08721e5a82d86680a57cf22085d9632ccb0eaf78
Vipin Kumar <vipinkumarx28@192.168.1.7>
2022-07-08 22:53:06 +0530

    saved file

--- api/client/src/pages/write/Write.jsx
+++ api/client/src/pages/write/Write.jsx
@@ -1,7 +1,7 @@
 import { useContext, useState } from "react";
 import "./write.css";
 import axios from "axios";
-import {axiosInstance} from "../../../config.js";
+import {axiosInstance} from "../../config.js";
 import { Context } from "../../context/Context";
 
 export default function Write() {
